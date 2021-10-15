cc42e7cc61bac331b7a31d141368c2b9ec9115cb
Tsugami <yslanramos.r@gmail.com>
2021-10-15 01:43:33 -0300

    feat: add user icon in post-card

--- components/PostCard.tsx
+++ components/PostCard.tsx
@@ -1,13 +1,19 @@
-import { Box } from '@chakra-ui/react';
+import { Box, Avatar, HStack, Heading, Text } from '@chakra-ui/react';
 
 const PostCard = (): JSX.Element => (
   <Box maxW='md' borderWidth='1px' borderRadius='lg' p='6'>
-    <Box>Title</Box>
-    <Box>
+    <Heading as='h4' size='md'>
+      Title
+    </Heading>
+    <Text my='3'>
       Lorem ipsum dolor, sit amet consectetur adipisicing elit. Neque aut debitis odit ab nisi,
       voluptates quod optio iusto illum alias illo dolor dolorum voluptatum animi nobis earum
       corrupti excepturi fugit?
-    </Box>
+    </Text>
+    <HStack>
+      <Avatar size='sm' name='Dan Abrahmov' src='https://bit.ly/dan-abramov' />
+      <Heading fontSize='sm'>Dan Abrahmov</Heading>
+    </HStack>
   </Box>
 );
 

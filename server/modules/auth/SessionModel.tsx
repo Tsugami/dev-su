874d8de79608be04125a225f51f48e03ec51bb41
Tsugami <yslanramos.r@gmail.com>
2021-10-15 18:52:23 -0300

    feat: add mongodb

---
+++ server/modules/auth/SessionModel.tsx
@@ -0,0 +1,16 @@
+import { Schema, model, Document } from 'mongoose';
+import UserModel from '../user/UserModel';
+
+const SessionSchema = new Schema({
+  expires: { type: Number, required: true },
+  sessionToken: { type: String, required: true, unique: true },
+  userId: { type: String, required: true, ref: UserModel },
+});
+
+export interface ISession extends Document {
+  expires: number;
+  sessionToken: string;
+  userId: string;
+}
+
+export default model<ISession>('Session', SessionSchema);

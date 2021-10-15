f2d46f37819c2ea5ac4264eb1d1ed74e0a21ee76
Tsugami <yslanramos.r@gmail.com>
2021-10-15 02:45:40 -0300

    feat: Add edit post card

--- components/PostCard.tsx
+++ components/PostCard.tsx
@@ -1,10 +1,33 @@
-import { Box, Avatar, HStack, Heading, Text } from '@chakra-ui/react';
+import {
+  Box,
+  Avatar,
+  HStack,
+  Heading,
+  Text,
+  Menu,
+  MenuButton,
+  MenuList,
+  MenuItem,
+} from '@chakra-ui/react';
+import { EditIcon } from '@chakra-ui/icons';
 
 const PostCard = (): JSX.Element => (
   <Box maxW='md' borderWidth='1px' borderRadius='lg' p='6'>
-    <Heading as='h4' size='md'>
-      Title
-    </Heading>
+    <HStack justifyContent='space-between'>
+      <Heading as='h4' size='md' isTruncated>
+        Title
+      </Heading>
+      <Menu>
+        <MenuButton>
+          <EditIcon cursor='pointer' _hover={{ color: 'teal.500' }} />
+        </MenuButton>
+        <MenuList>
+          <MenuItem>Edit</MenuItem>
+          <MenuItem>Delete</MenuItem>
+        </MenuList>
+      </Menu>
+    </HStack>
+
     <Text my='3'>
       Lorem ipsum dolor, sit amet consectetur adipisicing elit. Neque aut debitis odit ab nisi,
       voluptates quod optio iusto illum alias illo dolor dolorum voluptatum animi nobis earum

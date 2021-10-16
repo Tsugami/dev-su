adf5bc391626852694769415697ffbd069c85389
Tsugami <yslanramos.r@gmail.com>
2021-10-16 00:21:01 -0300

    feat: start setup relay

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,15 +1,21 @@
+import type { AppProps } from 'next/app';
+
 import { ChakraProvider } from '@chakra-ui/react';
 import { SessionProvider } from 'next-auth/react';
+import { RelayEnvironmentProvider } from 'react-relay/hooks';
+
 import GithubCorner from 'react-github-corner';
 
-import type { AppProps } from 'next/app';
+import RelayEnvironment from '../relay/RelayEnvironment';
 
 function MyApp({ Component, pageProps: { session, ...pageProps } }: AppProps) {
   return (
     <SessionProvider session={session}>
       <ChakraProvider>
-        <Component {...pageProps} />
-        <GithubCorner href={process.env.NEXT_PUBLIC_REPO_URL} />
+        <RelayEnvironmentProvider environment={RelayEnvironment}>
+          <Component {...pageProps} />
+          <GithubCorner href={process.env.NEXT_PUBLIC_REPO_URL} />
+        </RelayEnvironmentProvider>
       </ChakraProvider>
     </SessionProvider>
   );

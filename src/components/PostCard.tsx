862eb8e361b40e11f98177f33891fe20b9b3c301
Tsugami <yslanramos.r@gmail.com>
2021-10-16 02:13:06 -0300

    feat: add post-card fragment

--- src/components/PostCard.tsx
+++ src/components/PostCard.tsx
@@ -1,41 +1,82 @@
+import type { PostCard_post$key } from './__generated__/PostCard_post.graphql';
+
 import {
   Box,
   Avatar,
   HStack,
+  VStack,
   Heading,
   Text,
   Menu,
   MenuButton,
   MenuList,
   MenuItem,
+  Skeleton,
+  SkeletonCircle,
+  SkeletonText,
 } from '@chakra-ui/react';
 import { EditIcon } from '@chakra-ui/icons';
+import { graphql, useFragment } from 'react-relay';
 
-const PostCard = (): JSX.Element => (
-  <Box maxW='md' borderWidth='1px' borderRadius='lg' p='6'>
-    <HStack justifyContent='space-between'>
-      <Heading as='h4' size='md' isTruncated>
-        Title
-      </Heading>
-      <Menu>
-        <MenuButton>
-          <EditIcon cursor='pointer' _hover={{ color: 'teal.500' }} />
-        </MenuButton>
-        <MenuList>
-          <MenuItem>Edit</MenuItem>
-          <MenuItem>Delete</MenuItem>
-        </MenuList>
-      </Menu>
-    </HStack>
+type Props = {
+  post: PostCard_post$key;
+};
 
-    <Text my='3'>
-      Lorem ipsum dolor, sit amet consectetur adipisicing elit. Neque aut debitis odit ab nisi,
-      voluptates quod optio iusto illum alias illo dolor dolorum voluptatum animi nobis earum
-      corrupti excepturi fugit?
-    </Text>
-    <HStack>
-      <Avatar size='sm' name='Dan Abrahmov' src='https://bit.ly/dan-abramov' />
-      <Heading fontSize='sm'>Dan Abrahmov</Heading>
+const PostCard = ({ post }: Props): JSX.Element => {
+  const data = useFragment(
+    graphql`
+      fragment PostCard_post on Post {
+        title
+        content
+        user {
+          name
+          image
+        }
+      }
+    `,
+    post,
+  );
+
+  return (
+    <Box maxW='md' borderWidth='1px' borderRadius='lg' p='6'>
+      <HStack justifyContent='space-between'>
+        <Heading as='h4' size='md' isTruncated>
+          {data.title}
+        </Heading>
+        <Menu>
+          <MenuButton>
+            <EditIcon cursor='pointer' _hover={{ color: 'teal.500' }} />
+          </MenuButton>
+          <MenuList>
+            <MenuItem>Edit</MenuItem>
+            <MenuItem>Delete</MenuItem>
+          </MenuList>
+        </Menu>
+      </HStack>
+
+      <Text my='3'>{data.content}</Text>
+      <HStack>
+        <Avatar size='sm' name={data.user.name} src={data.user.image as string} />
+        <Heading fontSize='sm'>{data.user.name}</Heading>
+      </HStack>
+    </Box>
+  );
+};
+
+export const PostCardSkeleton = () => (
+  <Box maxW='md' borderWidth='sm' borderRadius='lg' p='6' w='full' boxShadow='sm'>
+    <VStack justifyContent='start' align='start'>
+      <Skeleton maxW='xs'>username</Skeleton>
+      <SkeletonText mt='4' noOfLines={4} spacing='4'>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Error rem esse quasi cumque labore
+        magni veritatis odio soluta dicta harum ipsa ad, sapiente, reprehenderit accusamus eaque
+        modi cupiditate dolore vel?
+      </SkeletonText>
+    </VStack>
+
+    <HStack mt='6'>
+      <SkeletonCircle size='5' />
+      <Skeleton h='3'>username</Skeleton>
     </HStack>
   </Box>
 );
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,22 +1,28 @@
 import { Flex, VStack } from '@chakra-ui/layout';
 import Header from '../components/Header';
 import type { NextPage } from 'next';
-import PostCard from '../components/PostCard';
+import { PostCardSkeleton } from '../components/PostCard';
+import { Suspense, lazy } from 'react';
+import dynamic from 'next/dynamic';
+
+const PostCard = lazy(() => import('../components/PostCard'), {});
 
 const Home: NextPage = () => {
   return (
     <Flex direction='column' alignItems='center' justifyContent='flex-start'>
       <Header />
       <VStack p='6' spacing='6'>
-        <PostCard />
-        <PostCard />
-        <PostCard />
-        <PostCard />
-        <PostCard />
-        <PostCard />
-        <PostCard />
-        <PostCard />
-        <PostCard />
+        <Suspense fallback={<PostCardSkeleton />}>
+          <PostCard />
+          <PostCard />
+          <PostCard />
+          <PostCard />
+          <PostCard />
+          <PostCard />
+          <PostCard />
+          <PostCard />
+          <PostCard />
+        </Suspense>
       </VStack>
     </Flex>
   );
